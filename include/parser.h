b917d91d34f1325172133f3a86e378becea6d3c4
Nicolas Rea <nrea@paul-f5Br6s1.clusters.42paris.fr>
2024-03-08 17:46:36 +0100

    parsing en cours
    Quasiment fini plus qu'a tester et debugger....

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: nrea <nrea@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 12:43:49 by nrea              #+#    #+#             */
-/*   Updated: 2024/03/08 16:15:58 by nrea             ###   ########.fr       */
+/*   Updated: 2024/03/08 17:08:42 by nrea             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,9 +33,9 @@ typedef struct s_node
 		{
 			int				in;
 			int				out;
-			t_token			*redir; /// A modifier
-			t_token			*cmd;  /// A modifier
-			char			*hd_buffer; /// A modifier
+			int				bultin;
+			t_token			*redir;
+			t_token			*cmd;
 		};
 		struct
 		{
@@ -47,9 +47,9 @@ typedef struct s_node
 
 int		ft_capture_here_doc(t_token *tok, char *eof);
 int		ft_redirections(t_token **stack);
-t_token	*ft_strip_bracket(t_token **stack);
+t_token	**ft_strip_bracket(t_token **stack);
 int		ft_check_outer_brackets(t_token *stack);
 int		ft_split_token_list(int rank, t_token **stack, t_token **l, t_token **r);
 int		ft_find_operator(int priority, t_token *stack);
-
+t_node	*ft_parse_tree(t_token **stack, int *error, int priority);
 #endif
