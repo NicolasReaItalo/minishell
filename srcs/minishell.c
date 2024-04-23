c0b106cba33ffe4913ec621a362c46d35b5de69a
Nicolas Rea <nrea@paul-f5Br5s6.clusters.42paris.fr>
2024-04-23 13:38:55 +0200

    Ajout de la fonction isatty pour forcer l'utilisation du shell en mode interactif

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: nrea <nrea@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 14:16:22 by tjoyeux           #+#    #+#             */
-/*   Updated: 2024/04/16 14:28:58 by nrea             ###   ########.fr       */
+/*   Updated: 2024/04/23 13:37:14 by nrea             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 void	exit_sadly(t_shell *shell, int exit_status)
 {
-	ft_dprintf(2, "\033[1;91mMinishell has exited du to an error\033[0m\n");
+	ft_dprintf(2, "\033[1;91mMinishell has exited due to an error\033[0m\n");
 	ft_free_shell(shell);
 	exit (exit_status);
 }
@@ -31,10 +31,12 @@ int	main(int argc, char **argv, char **envp)
 {
 	t_shell	shell;
 
-	if (set_interactive_signals() == -1)
-		exit(1);
 	(void) argc;
 	(void) argv;
+	if (!isatty(STDIN_FILENO) || !isatty(STDOUT_FILENO))
+		exit (1);
+	if (set_interactive_signals() == -1)
+		exit(1);
 	if (!ft_init_shell(&shell, envp))
 		return (1);
 	if (!ft_strlen(ft_get_var_value("PATH", shell.env_vars, shell.shell_vars)))
