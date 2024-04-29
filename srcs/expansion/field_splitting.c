f7d87b8fec4c6488bf28799dab24c2a7747ba441
Nicolas Rea <nrea@paul-f5Br1s1.clusters.42paris.fr>
2024-04-29 11:34:32 +0200

    le pb de VAR only IFS est regle par contre on aun souci quqnd VAR conteint une autre variable qui devrait etre expansee

--- srcs/expansion/field_splitting.c
+++ srcs/expansion/field_splitting.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   field_splitting.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tjoyeux <tjoyeux@student.42.fr>            +#+  +:+       +#+        */
+/*   By: nrea <nrea@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 14:56:40 by tjoyeux           #+#    #+#             */
-/*   Updated: 2024/04/26 17:03:10 by tjoyeux          ###   ########.fr       */
+/*   Updated: 2024/04/29 10:55:54 by nrea             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ void	free_words_tab(char ***words)
 int	get_last_word(char **words)
 {
 	int	i;
-	
+
 	if (!*words)
 		return (-1);
 	i = 0;
@@ -70,25 +70,37 @@ int	field_splitting(t_token *token, t_shell *shell, char *new, char *start, char
 	t_token	*ptr;
 	int		last;
 	char	*tmp;
-	char	*tmp2;	
+	char	*tmp2;
 
 
 
 	ifs = ft_get_var_value("IFS", shell->env_vars, shell->shell_vars);
 	words = ft_split_multiple(new, ifs);
-	if (!words)
-		return (1);
-	last = get_last_word(words);
-	if (last == -1)
-		return (free(words), 0);
-	tmp2 = ft_strjoin(start, words[0]);
 	ptr = token;
-	tmp = ft_strjoin(words[last], end);
-	free (words[last]);
-	words[last] = tmp;
-	words_to_token(ptr, words);
+	if (words == NULL)
+		return (1);
+	if (words[0])
+	{
+		last = get_last_word(words);
+		if (last == -1)
+			return (free(words), 0);
+		tmp2 = ft_strjoin(start, words[0]); // a proteger
+		tmp = ft_strjoin(words[last], end); // a proteger
+		free (words[last]);
+		words[last] = tmp;
+	}
+	else
+	{
+		free_words_tab(&words);
+		words = malloc(3 * sizeof(char*)); // a proteger
+		tmp2 = ft_strdup(start);
+		words[0] = ft_strdup("");
+		words[1] = ft_strdup(end);
+		words[2] = NULL;
+	}
+	words_to_token(ptr, words); // a proteger
+	free_words_tab(&words);
 	free(token->content);
 	token->content = tmp2;
-	free_words_tab(&words);
 	return (0);
 }
